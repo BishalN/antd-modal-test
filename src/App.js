c1e0b501bd82f26bcd79194769a49c2610913b73
Bishal Neupane <neupanebishal07@gmail.com>
2021-12-20 16:25:18 +0545

    Just good enough

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Modal, Input, Select, Space, Switch } from 'antd';
+import 'antd/dist/antd.min.css';
+import { useState } from 'react';
+
+const { Option } = Select;
+const { TextArea } = Input;
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type='primary' onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title='Report Query - BI annual Statement ..'
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key='back' onClick={handleOk}>
+            Save
+          </Button>,
+          <Button key='restore' onClick={handleOk}>
+            Restore
+          </Button>,
+          <Button key='ok' onClick={handleOk}>
+            Ok
+          </Button>,
+          <Button key='cancel'>Cancel</Button>,
+        ]}
+      >
+        <Space direction='vertical'>
+          <Space>
+            Field
+            <Select defaultValue='Empnumber' style={{ width: '100%' }}>
+              <Option value='Empnumber'>Empnumber</Option>
+              <Option value='OtherOptions'>OtherOptions</Option>
+              <Option value='Another Options'>Another Options</Option>
+              <Option value='Yet anohter one'>Yet anohter one</Option>
+            </Select>
+          </Space>
+          <Space>
+            Operator
+            <Switch
+              defaultChecked
+              checkedChildren='Not'
+              unCheckedChildren='Yes'
+            />
+            <Select defaultValue='Is Equal to'>
+              <Option value='Is Equal to'>Is Equal to</Option>
+              <Option value='OtherOptions'>OtherOptions</Option>
+              <Option value='Another Options'>Another Options</Option>
+              <Option value='Yet anohter one'>Yet anohter one</Option>
+            </Select>
+            Ignore Case
+            <Switch checkedChildren='Not' unCheckedChildren='Yes' />
+          </Space>
+          <Input addonBefore='value' />
+          Condition Specified
+          <TextArea rows={4} />
+        </Space>
+      </Modal>
     </div>
   );
 }
